7fa2f22795cd0018e5869bc013319c755f3d7807
binhoo <fabiogomesc7@gmail.com>
2024-05-19 06:30:16 -0300

    Ajustar Views e Controllers para inserção completa de Livros

---
+++ Library-Management-System/Library-Management-System/Controllers/PadraoController.cs
@@ -0,0 +1,114 @@
+﻿using Library_Management_System.DAO;
+using Library_Management_System.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace Library_Management_System.Controllers
+{
+    public class PadraoController<T> : Controller where T : PadraoViewModel
+    {
+        protected bool ExigeAutenticacao { get; set; } = true;
+        protected PadraoDAO<T> DAO { get; set; }
+        protected string NomeViewIndex { get; set; } = "index";
+        protected string NomeViewForm { get; set; } = "form";
+        protected virtual void ValidaDados(T model, string operacao) { }
+        protected virtual void PreencheDadosParaView(string Operacao, T model) { }
+
+        public virtual IActionResult Index()
+        {
+            try
+            {
+                var lista = DAO.Listagem();
+                return View(NomeViewIndex, lista);
+            }
+            catch (Exception erro)
+            {
+                return View("Error", new ErrorViewModel(erro.ToString()));
+            }
+        }
+        public virtual IActionResult Create()
+        {
+            try
+            {
+                ViewBag.Operacao = "I";
+                T model = Activator.CreateInstance<T>();
+                PreencheDadosParaView("I", model);
+                return View(NomeViewForm, model);
+            }
+            catch (Exception erro)
+            {
+                return View("Error", new ErrorViewModel(erro.ToString()));
+            }
+        }
+        public virtual IActionResult Save(T model, string Operacao)
+        {
+            try
+            {
+                ValidaDados(model, Operacao);
+                if (ModelState.IsValid == false)
+                {
+                    ViewBag.Operacao = Operacao;
+                    PreencheDadosParaView(Operacao, model);
+                    return View(NomeViewForm, model);
+                }
+                else
+                {
+                    if (Operacao == "I")
+                        DAO.Insert(model);
+                    else
+                        DAO.Update(model);
+                    return RedirectToAction(NomeViewIndex);
+                }
+            }
+            catch (Exception erro)
+            {
+                return View("Error", new ErrorViewModel(erro.ToString()));
+            }
+        }
+        public IActionResult Edit(int id)
+        {
+            try
+            {
+                ViewBag.Operacao = "A";
+                var model = DAO.Consulta(id);
+                if (model == null)
+                    return RedirectToAction(NomeViewIndex);
+                else
+                {
+                    PreencheDadosParaView("A", model);
+                    return View(NomeViewForm, model);
+                }
+            }
+            catch (Exception erro)
+            {
+                return View("Error", new ErrorViewModel(erro.ToString()));
+            }
+        }
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                DAO.Delete(id);
+                return RedirectToAction(NomeViewIndex);
+            }
+            catch (Exception erro)
+            {
+                return View("Error", new ErrorViewModel(erro.ToString()));
+            }
+
+        }
+
+        /*
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (ExigeAutenticacao && !HelperController.VerificaUserLogado(HttpContext.Session))
+                context.Result = RedirectToAction("Index", "Login");
+            else
+            {
+                ViewBag.Logado = true;
+                base.OnActionExecuting(context);
+            }
+        } */
+    }
+}
--- Library-Management-System/Library-Management-System/Models/BookViewModel.cs
+++ Library-Management-System/Library-Management-System/Models/BookViewModel.cs
@@ -1,19 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 
-namespace Library_Management_System.Models {
+namespace Library_Management_System.Models
+{
     public class BookViewModel : PadraoViewModel {
         public int AuthorId { get; set; }
         public int CategoryId { get; set; }
         public string Title { get; set; }
         public string ISBN { get; set; }
         public int PublishedYear { get; set; }
-        public byte[] Image { get; set; }
+
+        /// <summary>
+        /// Imagem recebida do form pelo controller
+        /// </summary>
+        public IFormFile Image { get; set; }
+
 
         // campos que não existem fisicamente na tabela de Book
         public string AuthorName { get; set; }
         public string CategoryDescription { get; set; }
+
+        /// <summary>
+        /// Imagem em bytes pronta para ser salva
+        /// </summary>
+        public byte[] ImageByte { get; set; }
+
+        /// <summary>
+        /// Imagem usada para ser enviada ao form no formato para ser exibida
+        /// </summary>
+        public string ImageBase64
+        {
+            get
+            {
+                if (ImageByte != null)
+                    return Convert.ToBase64String(ImageByte);
+                else
+                    return string.Empty;
+            }
+        }
+
     }
 }
